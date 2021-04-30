36b24f374f3a83a1e1bef8c1526f9dbefb996a7c
Roshan Pratap Katel <paradise98425@gmail.com>
2021-04-30 14:57:52 +0545

    basics of select antd made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Select as SelectBox, Divider, Input, Button } from 'antd';
+import { useState } from 'react';
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState(["Roshan", "pratap", "katel"]);
+  const [OptionOpen, setOptionOpen] = useState(false)
+  const { Option } = SelectBox;
+  
+  const onItemChange = (event) => {
+    setNewItem(event.target.value);
+  };
+  const addItem = (event) => {
+    if (event.keyCode == 13) {
+      if (!items.includes(newItem)) {
+        setItems([items[0], newItem, ...items.slice(1, items.length)]);
+        setNewItem("");
+      } else {
+        setNewItem("");
+      }
+    }
+  };
+
+  const handleSelectClick = (event) => {
+    setOptionOpen(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SelectBox
+        style={{ width: 240 }}
+        placeholder="custom dropdown render"
+        onClick={handleSelectClick}
+        open={OptionOpen}
+        dropdownRender={menu => (
+          <div>
+            {menu}
+            <Divider style={{ margin: '4px 0' }} />
+            <Input
+              value={newItem}
+              onChange={onItemChange}
+              onKeyDown={addItem}
+              maxLength={5}
+            />
+          </div>
+        )}
+      >
+        {items.map(item => (
+          <Option key={item}>{item}</Option>
+        ))}
+      </SelectBox>
     </div>
   );
 }
