e1a4d94f085c19f164c56135ecb9a181636b0c5c
Roshan Pratap Katel <paradise98425@gmail.com>
2021-04-30 15:46:23 +0545

    value of select box replaced

--- src/App.js
+++ src/App.js
@@ -5,27 +5,32 @@ function App() {
   const [newItem, setNewItem] = useState("");
   const [items, setItems] = useState(["Roshan", "pratap", "katel"]);
   const { Option } = SelectBox;
+  const [selectedValue, setSelectedValue] = useState("")
+
   
   const onItemChange = (event) => {
+    console.log("Clicked", event.target.value)
     setNewItem(event.target.value);
   };
   const addItem = (event) => {
     if (event.keyCode == 13) {
       if (!items.includes(newItem)) {
         setItems([items[0], newItem, ...items.slice(1, items.length)]);
+        setSelectedValue(newItem);
         setNewItem("");
       } else {
+        setSelectedValue(newItem);
         setNewItem("");
       }
     }
   };
-
 
   return (
     <div className="App">
       <SelectBox
         style={{ width: 240 }}
         placeholder="custom dropdown render"
+        value={selectedValue}
         dropdownRender={menu => (
           <div>
             {menu}
